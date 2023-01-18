0fa716c0202e026107151d33a727e3569981ede0
DamienBuchet <damienbuchetdb@gmail.com>
2023-01-18 14:45:57 +0100

    Added Faker to generate data

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Tweet;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Faker;
+use Doctrine\Persistence\ManagerRegistry;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): JsonResponse
+    public function index(ManagerRegistry $doctrine): JsonResponse
     {
+        // $em = $doctrine->getManager();
+        // $faker = Faker\Factory::create('fr_FR');
+        // $populator = new Faker\ORM\Doctrine\Populator($faker, $em);
+        // $populator->addEntity(User::class, 10);
+        // $populator->addEntity(Tweet::class, 5);
+        // $insertedPKs = $populator->execute();
         return $this->json([
             'message' => 'Hello World!',
         ]);
---
+++ api/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tweet;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        $faker->seed(1717);
+
+        $users = [];
+        for ($i=0; $i < 10; $i++) {
+            $users[$i] = new User();
+            $users[$i]
+                ->setPseudo($faker->firstNameMale)
+                ->setPassword($faker->password())
+                ->setProfilPic(NULL)
+                ->setDescription($faker->realText())
+                ->setSuivis(NULL)
+                ->setLikes(NULL)
+            ;
+            $manager->persist($users[$i]);
+        }
+
+        $tweets = [];
+        for ($i=0; $i < 15; $i++) { 
+            $tweets[$i] = new Tweet();
+            $tweets[$i]
+                ->setDateheure($faker->dateTime)
+                ->setMessage($faker->realText())
+                ->setMedia(NULL)
+            ;
+            $randomusers = (array) array_rand($users, rand(1, count($users)));
+
+            foreach ($randomusers as $key => $value) {
+                $tweets[$i]->setUser($users[$key]);
+            }
+
+            $manager->persist($tweets[$i]);
+        }
+
+        $manager->flush();
+    }
+}
